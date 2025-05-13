47a9b020f03034e6f30e43c7c1714e77915265e6
Toaa Mahmoud <toaamahmoud1642003@gmail.com>
2025-05-14 01:20:17 +0300

    feat: add search by image feature ai

---
+++ src/search_by_image/search.py
@@ -0,0 +1,91 @@
+import os
+import shutil
+import logging
+import asyncio
+from pathlib import Path
+from fastapi import FastAPI, File, UploadFile
+from DeepImageSearch import Load_Data, Search_Setup
+from contextlib import asynccontextmanager
+
+from src.search_by_image.image_utils import fetch_image_urls, download_images
+os.chdir(Path(__file__).resolve().parent)
+
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+BASE_DIR = Path(__file__).resolve().parent  # src/search-by-image
+
+IMAGE_DIR = BASE_DIR / "data"
+UPLOAD_FOLDER = BASE_DIR / "uploads"
+
+Path(IMAGE_DIR).mkdir(parents=True, exist_ok=True)
+Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+search_engine = None
+
+async def update_index_with_new_images():
+    image_data = await fetch_image_urls()  
+    await download_images(image_data)  
+    image_list = Load_Data().from_folder([IMAGE_DIR])  
+    search_engine.run_index()  
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global search_engine
+    if os.path.exists('.deep_image_search'):
+        shutil.rmtree('.deep_image_search')
+
+    # Fetch and download images during startup
+    image_data = await fetch_image_urls()
+    await download_images(image_data)
+    
+    # Load and index the images
+    image_list = Load_Data().from_folder([IMAGE_DIR])
+    logger.info(f"Total images indexed: {len(image_list)}")
+
+    search_engine = Search_Setup(image_list=image_list)
+    search_engine.run_index()
+
+    # Start background task to update index with new images
+    asyncio.create_task(update_index_with_new_images())
+
+    # Yield to indicate that the startup process is complete
+    yield
+    
+    # Shutdown tasks (if needed)
+    logger.info("Shutting down the application")
+
+# Create the FastAPI app with the lifespan context manager
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI is running with image search!"}
+
+@app.post("/search-by-image/")
+async def search_by_image(file: UploadFile = File(...), top_n: int = 5):
+    file_location = f"{UPLOAD_FOLDER}/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    if search_engine is None:
+        raise Exception("Search engine not initialized")  
+
+    search_results = search_engine.get_similar_images(image_path=file_location, number_of_images=top_n)
+    meal_ids = []
+    for path in search_results.values():
+        filename = os.path.basename(path)
+        if filename.startswith("meal_") and filename.endswith(".jpg"):
+            meal_id = filename.replace("meal_", "").replace(".jpg", "")
+            meal_ids.append(meal_id)
+
+    return {
+        "query_image": file_location,
+        "similar_meal_ids": meal_ids
+    }
+    
+    
+# uvicorn src.search-by-image.search:app --reload     
