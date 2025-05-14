348574141c7df5c699413f4b4346063ad8cfa8ee
Toaa Mahmoud <toaamahmoud1642003@gmail.com>
2025-05-14 03:45:13 +0300

    core: testing the code

--- index.py
+++ index.py
@@ -4,17 +4,18 @@ from src.search_by_image.search import app as search_app, lifespan
 main_app = FastAPI()
 
 # Mount the inner app under a prefix
-main_app.mount("/search", search_app)
+# main_app.mount("/search", search_app)
 
 # Root-level route for testing
+print("Hello There")
 @main_app.get("/")
 def root():
     return {"message": "Main App is running"}
 # Handle the startup of the mounted app manually
-@main_app.on_event("startup")
+''' @main_app.on_event("startup")
 async def start_inner_app():
     # Run the lifespan startup manually
-    await lifespan(search_app).__aenter__()
+    await lifespan(search_app).__aenter__() '''
 
 
 #uvicorn index:main_app --reload
