529d3cf5c430f4009be051663ad3125369c31d59
Toaa Mahmoud <toaamahmoud1642003@gmail.com>
2025-06-25 18:35:27 +0300

    Update main file

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from src.search_by_image.search import app as search_app
 
 main_app = FastAPI()
 
-# Mount the inner app under a prefix
-main_app.mount("/search", search_app)
-
 # Root-level route for testing
 @main_app.get("/")
 def root():
     return {"message": "Main App is running"}
 
+# Mount the inner app under a prefix
+main_app.mount("/search", search_app)
+
 #  uvicorn main:main_app --reload
 #  uvicorn main:main_app --host 0.0.0.0 --port 8000
-#
+#  python -m uvicorn main:main_app --host 0.0.0.0 --port 8000
