adcefe3ec9f442b15b2f95d5b9fcc14775e338ab
K7413dS433d <khaledsaied2002@gmail.com>
2025-06-25 16:49:56 +0300

    return to last stable version

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn main:main_app -k uvicorn.workers.UvicornWorker
--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Start the application using Gunicorn
-gunicorn main:main_app -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT
